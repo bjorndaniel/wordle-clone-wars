cca39e29ef790a502f825c3fd93fb8b9dccf2f76
Daniel Sandberg <bjorndaniel@gmail.com>
2022-03-14 18:22:17 +0100

    Refresh stats on save

---
+++ src/WordleCloneWars/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿namespace WordleCloneWars.Services;
+
+public class UserService
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly AuthenticationStateProvider _authProvider;
+    private readonly ILogger<RoundService> _logger;
+    
+    public UserService(ApplicationDbContext dbContext, AuthenticationStateProvider authProvider, ILogger<RoundService> logger)
+    {
+        _dbContext = dbContext;
+        _authProvider = authProvider;
+        _logger = logger;
+    }
+
+    public async Task<List<User>> GetAllAsync()
+    {
+        var state = await _authProvider.GetAuthenticationStateAsync();
+        if (state?.User?.IsInRole("Administrator") ?? false)
+        {
+            return await _dbContext.Users.OrderBy(_ => _.FirstName).ThenBy(_ => _.LastName).ToListAsync();
+        }
+
+        return new List<User>();
+    }
+}
