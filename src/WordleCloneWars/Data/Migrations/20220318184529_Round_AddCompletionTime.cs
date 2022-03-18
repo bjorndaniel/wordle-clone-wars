aaf24653fcb03f3b4b0816747b232f45dd823106
Daniel Sandberg <=>
2022-03-18 20:40:31 +0100

    #23

---
+++ src/WordleCloneWars/Data/Migrations/20220318184529_Round_AddCompletionTime.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WordleCloneWars.Data.Migrations;
+
+public partial class Round_AddCompletionTime : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<long>(
+            name: "CompletedDateTime",
+            table: "Rounds",
+            type: "INTEGER",
+            nullable: false,
+            defaultValue: 0L);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "CompletedDateTime",
+            table: "Rounds");
+    }
+}
