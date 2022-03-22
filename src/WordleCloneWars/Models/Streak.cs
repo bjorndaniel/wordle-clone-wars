beb25fc8249d8fc42093da2cfbcb8d9badbeb38c
Daniel Sandberg <bjorndaniel@gmail.com>
2022-03-22 20:13:40 +0100

    Added active streak to main
    #22

---
+++ src/WordleCloneWars/Models/Streak.cs
@@ -0,0 +1,9 @@
+﻿namespace WordleCloneWars.Models;
+
+public class Streak
+{
+    public GameType Type { get; set; }
+    public int Rounds { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string DisplayText => string.IsNullOrWhiteSpace(Username) ? $"{Type}: No one has a streak recorded" : $"{Type}: {Username} has a {Rounds} round streak";
+}
