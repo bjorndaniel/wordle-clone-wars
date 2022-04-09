adb4469c5ee839b7ac65046e2e4d72937c2fea75
Daniel Sandberg <bjorndaniel@gmail.com>
2022-04-09 11:02:54 +0200

    First sparkline version

--- src/WordleCloneWars/wwwroot/js/site.js
+++ src/WordleCloneWars/wwwroot/js/site.js
@@ -1,23 +1,31 @@
-﻿function setupPasteEvent(helper){
+﻿function setupPasteEvent(helper) {
     const textArea = document.getElementById("roundData");
     textArea.addEventListener("paste", async (e) => {
-       let paste = (e.clipboardData || window.clipBoardData).getData("text");
-       await helper.invokeMethodAsync("CheckResult", paste);
+        let paste = (e.clipboardData || window.clipBoardData).getData("text");
+        await helper.invokeMethodAsync("CheckResult", paste);
     });
 }
 
-function switchTab(id){
+function switchTab(id) {
     const elements = document.getElementsByClassName("tab-pane");
     const elementsBtn = document.getElementsByClassName("nav-link");
     const target = document.getElementById(id);
     const targetBtn = document.getElementById(`btn${id}`);
-    
+
     Array.from(elements).map((t) => {
-            t.classList.remove("show");  
-            t.classList.remove("active");  
-        } );
+        t.classList.remove("show");
+        t.classList.remove("active");
+    });
     Array.from(elementsBtn).map((b) => b.classList.remove("active"));
     target.classList.add("show");
     target.classList.add("active");
     targetBtn.classList.add("active");
+}
+
+function getWidth(id) {
+    console.log(id);
+    const element = document.getElementById(id);
+    const width = element?.clientWidth || 0;
+    console.log(width);
+    return width;
 }
